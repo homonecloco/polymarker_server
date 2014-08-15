892e46b99c2a32de59519e4d4591f37a719f134e
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2014-08-15 12:07:01 +0100

    Added message about identity. Sanity of the SNPs is verified.

---
+++ src/main/java/ac/uk/tgac/compgen/PolymarkerException.java
@@ -0,0 +1,11 @@
+package ac.uk.tgac.compgen;
+
+/**
+ * Created by ramirezr on 15/08/2014.
+ */
+
+public class PolymarkerException extends Exception {
+    public PolymarkerException(String message) {
+            super(message);
+        }
+}
